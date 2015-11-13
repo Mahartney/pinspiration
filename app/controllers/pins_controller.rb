854d139b62bd8dc714b1599cea379f57f8be0328
Chase Gruber <chasegruber@gmail.com>
2015-11-13 11:34:06 -0500

    chase work on pin controller and views

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,2 +1,49 @@
 class PinsController < ApplicationController
+
+#   board_pins GET    /boards/:board_id/pins(.:format)          pins#index
+#            POST   /boards/:board_id/pins(.:format)          pins#create
+# new_board_pin GET    /boards/:board_id/pins/new(.:format)      pins#new
+# edit_board_pin GET    /boards/:board_id/pins/:id/edit(.:format) pins#edit
+#  board_pin GET    /boards/:board_id/pins/:id(.:format)      pins#show
+#            PATCH  /boards/:board_id/pins/:id(.:format)      pins#update
+#            PUT    /boards/:board_id/pins/:id(.:format)      pins#update
+#            DELETE /boards/:board_id/pins/:id(.:format)      pins#destroy
+
+  def index
+    @board = Board.find(params[:board_id])
+    redirect_to board_path(@board)
+  end
+
+  def create
+    @board = Board.find(params[:board_id])
+    @pin = @board.pins.create(pin_params)
+    redirect_to board_path(@board)
+  end
+
+  def new
+    @board = Board.find(params[:board_id])
+    @pin = Pin.new
+  end
+
+  def edit
+    @board = Board.find(params[:board_id])
+    @pin = Pin.find(params[:id])
+  end
+
+  def update
+    @board = Board.find(params[:board_id])
+  end
+
+  def destroy
+    @pin = Pin.find(params[:id])
+    @pin.destroy
+  end
+
+
+  private
+  def pin_params
+    params.require(:pin).permit(:title,:image_url)
+  end
+
+
 end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,2 +1,5 @@
 class Pin < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :board
+  has_many :tags
 end
