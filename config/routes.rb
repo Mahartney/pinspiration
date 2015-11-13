953f7c5bd8b1965591f6b8d9537a1d1de3c4c545
Chase Gruber <chasegruber@gmail.com>
2015-11-13 15:52:28 -0500

    filter posts by tags, add tag index

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :tags
+  resources :captions, only: [:index, :show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
